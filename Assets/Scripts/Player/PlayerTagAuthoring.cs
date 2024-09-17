9a9fda3a6d3e7d8b232340e712acd0c205d71791
goldfinch <dyboshit@gmail.com>
2024-09-17 14:04:32 +0300

    feat: tags authoring

---
+++ Assets/Scripts/Player/PlayerTagAuthoring.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Player {
+    public class PlayerTagAuthoring : MonoBehaviour {
+        private class PlayerTagAuthoringBaker : Baker<PlayerTagAuthoring> {
+            public override void Bake(PlayerTagAuthoring authoring) {
+                Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new PlayerTag());
+            }
+        }
+    }
+}
