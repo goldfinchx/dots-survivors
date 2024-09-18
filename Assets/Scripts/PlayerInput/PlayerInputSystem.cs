a1f67ee006708f690bab7d5fb0fe2dd5fbf80080
goldfinch <dyboshit@gmail.com>
2024-09-18 14:16:17 +0300

    feat: implemented player input

---
+++ Assets/Scripts/PlayerInput/PlayerInputSystem.cs
@@ -0,0 +1,51 @@
+﻿using Movement;
+using NUnit.Framework;
+using Player;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PlayerInput {
+
+    [UpdateInGroup(typeof(InitializationSystemGroup), OrderLast = true)]
+    public partial class PlayerInputSystem : SystemBase {
+
+        private Entity playerEntity;
+        private PlayerInputActions actions;
+        private InputAction moveAction;
+
+        protected override void OnCreate() {
+            RequireForUpdate<PlayerTag>();
+
+            actions = new PlayerInputActions();
+            moveAction = actions.Player.Move;
+        }
+
+        protected override void OnStartRunning() {
+            playerEntity = SystemAPI.GetSingletonEntity<PlayerTag>();
+            actions.Enable();
+        }
+
+        protected override void OnStopRunning() {
+            actions.Disable();
+        }
+
+        protected override void OnUpdate() {
+            float2 input = moveAction.ReadValue<Vector2>();
+            UpdateMovement(input);
+        }
+
+        private void UpdateMovement(float2 input) {
+            MovementComponent movement = EntityManager.GetComponentData<MovementComponent>(playerEntity);
+            LocalTransform transform = EntityManager.GetComponentData<LocalTransform>(playerEntity);
+
+            float2 newTarget = transform.Position.xy + input;
+            movement.Target = newTarget;
+
+            EntityManager.SetComponentData(playerEntity, movement);
+        }
+
+    }
+}
