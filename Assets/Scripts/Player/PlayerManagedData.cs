45038c64d7f96bf10a3d223683fc325febb01f2d
goldfinch <dyboshit@gmail.com>
2024-09-23 17:47:59 +0300

    feat: player presentation system

---
+++ Assets/Scripts/Player/PlayerManagedData.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Player {
+    public class PlayerManagedData : IComponentData {
+        public GameObject PresenterGameObject;
+    }
+}
---
+++ Assets/Scripts/PlayerPresentation/PlayerPresentationSystem.cs
@@ -0,0 +1,38 @@
+﻿using Player;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace PlayerPresentation {
+    public partial class PlayerPresentationSystem : SystemBase {
+     
+        protected override void OnCreate() {
+            base.OnCreate();
+            RequireForUpdate<PlayerTag>();
+        }
+
+        protected override void OnUpdate() {
+            Entity playerEntity = SystemAPI.GetSingletonEntity<PlayerTag>();
+            if (!EntityManager.HasComponent<PlayerManagedData>(playerEntity)) {
+                CreatePresentationObject(ref playerEntity);
+                return;
+            }
+            
+            SyncPresentationObjectTransform(ref playerEntity);
+        }
+        
+        private void CreatePresentationObject(ref Entity playerEntity) {
+            GameObject playerPresentation = new("PlayerPresentation");
+            PlayerManagedData playerManagedData = new() { PresenterGameObject = playerPresentation };
+            EntityManager.AddComponentObject(playerEntity, playerManagedData);
+        }
+        
+        private void SyncPresentationObjectTransform(ref Entity playerEntity) {
+            PlayerManagedData playerManagedData = EntityManager.GetComponentData<PlayerManagedData>(playerEntity);
+            LocalTransform localTransform = EntityManager.GetComponentData<LocalTransform>(playerEntity);
+            playerManagedData.PresenterGameObject.transform.position = localTransform.Position;
+        }
+    }
+    
+    
+}
