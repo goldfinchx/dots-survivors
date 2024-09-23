1bc75125a06d0144734b908a4792248628afa009
goldfinch <dyboshit@gmail.com>
2024-09-23 17:49:24 +0300

    feat: camera system

---
+++ Assets/Scripts/Camera/CameraSetupSystem.cs
@@ -0,0 +1,29 @@
+﻿using Cinemachine;
+using Player;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Camera {
+    public partial class CameraSetupSystem : SystemBase {
+        
+        private CinemachineVirtualCamera virtualCamera;
+
+        protected override void OnCreate() {
+            base.OnCreate();
+            RequireForUpdate<PlayerTag>();
+            RequireForUpdate<PlayerManagedData>();
+        }
+
+        protected override void OnUpdate() {
+            if (virtualCamera is not null) {
+                return;
+            }
+
+            Entity playerEntity = SystemAPI.GetSingletonEntity<PlayerTag>();
+            PlayerManagedData playerManagedData = EntityManager.GetComponentData<PlayerManagedData>(playerEntity);
+            virtualCamera = Object.FindFirstObjectByType<CinemachineVirtualCamera>();
+            virtualCamera.Follow = playerManagedData.PresenterGameObject.transform;
+        }
+        
+    }
+}
